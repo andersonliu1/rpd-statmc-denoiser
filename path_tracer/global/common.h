15c85beb295f78794dc965449b4ae2a4b6e37147
Anderson Liu <agliu@usc.edu>
2025-11-14 10:44:27 -0800

    starting

---
+++ path_tracer/global/common.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <Eigen/Dense>
+
+#define EPS 0.001f
+
+template<int N, typename T> using Vec = Eigen::Matrix<T, N, 1>;
+
+template<class T> using Vec2 = Vec<2, T>;
+template<class T> using Vec3 = Vec<3, T>;
+template<class T> using Vec4 = Vec<4, T>;
+
+using Vec2f = Vec2<float>;
+using Vec3f = Vec3<float>;
+using Vec4f = Vec4<float>;
+
+using Vec2i = Vec2<int>;
+using Vec3i = Vec3<int>;
+
+template<typename T, int M, int N> using Mat = Eigen::Matrix<T, M, N>;
+
+template<class T> using Mat2 = Mat<T, 2, 2>;
+template<class T> using Mat3 = Mat<T, 3, 3>;
+template<class T> using Mat4 = Mat<T, 4, 4>;
+
+using Mat2f = Mat2<float>;
+using Mat3f = Mat3<float>;
+using Mat4f = Mat4<float>;
---
+++ path_tracer/global/sampler.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "common.h"
+#include <cmath>
+#include <random>
+
+struct Sampler {
+    inline static std::mt19937 rng;
+    inline static std::uniform_real_distribution<float> dist;
+
+    static void init(uint32_t seed) {
+        rng = std::mt19937(seed);
+        dist = std::uniform_real_distribution<float>(0.0f, 1.0f);
+    }
+
+    static float next1d() {
+        return dist(rng);
+    }
+
+    static Vec2f next2d() {
+        return Vec2f(next1d(), next1d());
+    }
+
+    static Vec2f sample_disk(){
+        Vec2f u = next2d();
+        Vec2f offset = 2.0f * u - Vec2f::Ones();
+        if(offset.x() == 0 || offset.y() == 0) return Vec2f::Zero();
+        float theta, r;
+
+        if(abs(offset.x()) > abs(offset.y())){
+            r = offset.x();
+            theta = M_PI_4 * (offset.y() / offset.x());
+        }else{
+            r = offset.y();
+            theta = M_PI_2 - M_PI_4 * (offset.x() / offset.y());
+        }
+        return r * Vec2f(cos(theta), sin(theta));
+    }
+};
